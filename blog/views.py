4b31cbc53f966d9f3ba5e2988850df27a4d23fb8
yeck <yeck_oh@163.com>
2018-08-21 17:13:50 +0800

    基于类的通用视图

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 from .models import Post,Category
 from comments.forms import CommentForm
 import markdown
+from django.views.generic import ListView, DetailView
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 # Create your views here.
 def index(request):
     # print(request)
@@ -11,6 +18,51 @@ def index(request):
     # return render(request,'blog/index.html',context={'title':'我的博客首页','welcome':'欢迎访问我的博客主页'})
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return  response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super(PostDetailView, self).get_object(queryset=None)
+
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+
+        form = CommentForm()
+
+        comment_list = self.object.comment_set.all()
+
+        context.update({'form': form,
+                        'comment_list': comment_list
+                        })
+        return  context
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -29,12 +81,29 @@ def detail(request, pk):
                'comment_list':comment_list}
     return render(request,'blog/detail.html', context=context)
 
+class ArchivesView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(create_time__year = year,
+                                                               create_time__month = month)
+
 def archives(request, year, month):
+
     post_list = Post.objects.filter(create_time__year=year,
                                     create_time__month=month).order_by('-create_time')
 
     return render(request, 'blog/index.html', context={'post_list':post_list})
 
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk = self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
 def category(request, pk):
     cate = get_object_or_404(Category,pk=pk)
